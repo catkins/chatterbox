bda42f56db037307a2fcbed2ba303ab250c9b6c3
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-08-19 00:07:25 +1000

    Fluxify message and room stores

--- src/stores/message-store.js
+++ src/stores/message-store.js
@@ -1,6 +1,8 @@
+import AppDispatcher from '../dispatcher/app-dispatcher'
 import { EventEmitter } from 'events';
 
-const messages = {
+// dummy data for prototype
+const _messages = {
   general: [
     {
       from: 'gary',
@@ -14,7 +16,7 @@ const messages = {
 
   random: [
     {
-      from: 'murry',
+      from: 'murray',
       text: 'Oi paloi'
     }
   ]
@@ -22,10 +24,46 @@ const messages = {
 
 class MessageStore extends EventEmitter {
 
+  constructor(messages) {
+    super();
+    this.messages = messages;
+    this.registerWithDispatcher();
+  }
+
   getMessagesForRoom(roomName) {
-    return messages[roomName] || [];
+    return this.messages[roomName] || [];
+  }
+
+  registerWithDispatcher() {
+    this.dispatchToken = AppDispatcher.register((payload) => {
+
+      switch(payload.eventName) {
+        case 'create-message':
+          let { room, text } = payload.data;
+          let user = 'gary';
+
+          this.createMessage(room, text, user);
+          break;
+
+        default:
+          break;
+      }
+
+      return true;
+    });
+  }
+
+  createMessage(room, text, user) {
+    this.messages[room] = this.messages[room] || [];
+
+    this.messages[room].push({
+      from: user,
+      text: text
+    });
+
+    this.emit('change');
   }
 
 }
 
-export default new MessageStore();
+export default new MessageStore(_messages);
--- src/stores/room-store.js
+++ src/stores/room-store.js
@@ -1,19 +1,36 @@
+import AppDispatcher from '../dispatcher/app-dispatcher';
 import { EventEmitter } from 'events';
 
-const rooms = [
+// dummy data for prototype
+const _rooms = [
   { name: 'general' },
   { name: 'random'  }
 ]
 
 class RoomStore extends EventEmitter {
+
   constructor(rooms) {
     super();
-    this.rooms = rooms
+    this.rooms = rooms;
+    this.registerWithDispatcher();
   }
 
   getAllRooms() {
     return this.rooms;
   }
+
+  registerWithDispatcher() {
+    this.dispatchToken = AppDispatcher.register((payload) => {
+
+      switch(payload.eventName) {
+        default:
+          break;
+      }
+
+      return true;
+    });
+  }
+
 }
 
-export default new RoomStore(rooms);
+export default new RoomStore(_rooms);
