caa31dce29a85ab35c00fbeb1d734456fcc5b1cf
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-08-19 00:12:56 +1000

    Added input field and wired up room component to dispatch new messages via store

--- src/components/room.js
+++ src/components/room.js
@@ -1,31 +1,84 @@
-import React        from 'react';
-import MessageStore from '../stores/message-store';
+import React         from 'react';
+import MessageStore  from '../stores/message-store';
+import AppDispatcher from '../dispatcher/app-dispatcher';
 
 const { Component } = React
 
 class Room extends Component {
 
-  renderMessages() {
+  constructor(props) {
+    super(props);
+    this.state = { newMessage: '' };
+  }
+
+  componentDidMount() {
+    MessageStore.on('change', this._messagesChanged)
+  }
+
+  componentWillUnmount() {
+    MessageStore.removeListener('change', this._messagesChanged)
+  }
+
+  render() {
+    const messages = this._renderMessages();
+
+    return (
+      <div>
+        <h4>{messages.length} message{messages.length == 1 ? '' : 's' }</h4>
+
+        <ul>
+          {this._renderMessages()}
+        </ul>
+
+        {this._renderNewMessageBox()}
+
+      </div>
+    );
+  }
+
+  _renderMessages() {
     const roomId = this.props.params.room;
 
     let messages = MessageStore.getMessagesForRoom(roomId);
 
     return messages.map((msg, index) =>
-      (<li key={index}>{msg.from}: {msg.text}</li>)
+      (<li key={index}>@{msg.from}: {msg.text}</li>)
     );
   }
 
-  render() {
-    const messages = this.renderMessages();
-
+  _renderNewMessageBox() {
     return (
-      <div>
-        <h4>{messages.length} messages</h4>
-        <ul>
-          {messages}
-        </ul>
+      <div className="row collapse">
+        <div className="small large-10 columns">
+          <input type="text" value={this.state.newMessage} onChange={this._newMessageChanged} />
+        </div>
+        <div className="small large-2 columns">
+          <button className="button postfix" onClick={this._sendMessage}>Send</button>
+          </div>
       </div>
     );
+  }
+
+  _messagesChanged = () => {
+    // TODO: is there a nicer way of doing this?
+    this.forceUpdate();
+  }
+
+  _newMessageChanged = (event) => {
+    this.setState({ newMessage: event.target.value });
+  }
+
+  _sendMessage = (event) => {
+    let newMessage = this.state.newMessage;
+    this.setState({ newMessage: '' });
+
+    AppDispatcher.dispatch({
+      eventName: 'create-message',
+      data: {
+        text: newMessage,
+        room: this.props.params.room
+      }
+    });
   }
 }
 
